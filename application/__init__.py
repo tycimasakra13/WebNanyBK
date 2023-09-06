6dd54813c17ddbf51e0050352586b8f16b6e4eb1
aburczyn <asia.franczak@nokia.com>
2023-09-07 00:23:13 +0200

    db connect requirements up 2

--- application/__init__.py
+++ application/__init__.py
@@ -1,13 +1,15 @@
 import os
+from application import routes
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
+db = SQLAlchemy()
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DEVELOPMENT_DATABASE_URL")
 
-db = SQLAlchemy()
 db.init_app(app)
 
-from application import routes
+
