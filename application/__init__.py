f9065e48653f2991fd65fc6fd57c58690578382a
aburczyn <asia.franczak@nokia.com>
2023-09-06 14:39:41 +0200

    first version

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from config import Config
+from flask_postgresql import PostgreSQL
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = PostgreSQL()
+db.init_app(app)
 
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,50 @@
-from application import app
+from application import app, db
+from flask import render_template, request, Response, json
+
+test_data = [{"id": "1", "title": "test1"}, {"id": "2", "title": "test2"}]
+
+
 @app.route('/')
 def home():
     return 'Hello World'
 
+
 @app.route('/index')
+@app.route('/home')
 def index():
-    return 'Hello Earth'
+    return render_template("index.html", index=True)
+
+
+@app.route('/test/')
+@app.route('/test/<id>')
+def test(id="1"):
+    return render_template("test.html", testData=test_data, test=True, id=id)
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html", register=True)
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html", login=True)
+
+
+@app.route('/enrollment', methods=["GET", "POST"])
+def enrollment():
+    id = request.form.get('id')
+    title = request.form.get('title')
+    return render_template("enrollment.html", enrollment=True, data={"id": id, "title": title})
+
+
+@app.route("/api/")
+@app.route("/api/<idx>")
+def api(idx=None):
+    if (idx == None):
+        jdata = test_data
+    else:
+        jdata = test_data[int(idx)]
+
+    return Response(json.dumps(jdata), mimetype="application/json")
+
