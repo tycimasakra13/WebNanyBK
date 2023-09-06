81ea4dfcaa80292bd7500480f6e4ceba196cb193
aburczyn <asia.franczak@nokia.com>
2023-09-07 00:37:13 +0200

    db connect requirements up 5

--- application/__init__.py
+++ application/__init__.py
@@ -3,11 +3,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
-
-
 app = Flask(__name__)
+
 app.config.from_object(Config)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DEVELOPMENT_DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://webnany:Nanydb123@webnany.postgres.database.azure.com:5432/webnany"
 
 db = SQLAlchemy(app)
 
